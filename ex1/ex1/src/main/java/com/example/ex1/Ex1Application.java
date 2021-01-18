6958a2a5080599d031b1e239b93cb75ecfd8b44a
RATIONAL331 <rational331@gmail.com>
2021-01-18 21:09:34 +0900

    ch1 Finish

--- ex1/ex1/src/main/java/com/example/ex1/Ex1Application.java
+++ ex1/ex1/src/main/java/com/example/ex1/Ex1Application.java
@@ -9,5 +9,5 @@ public class Ex1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Ex1Application.class, args);
 	}
-
+	// gradle 탭에서 Tasks -> build -> bootJar를 더블 클릭하면 jar파일이 만들어지고 java -jar로 실행할 수 있다.
 }
